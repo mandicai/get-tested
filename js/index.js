fc02c61b4fa05550cebf7596ed0aa7ed5065fd9a
Mandi Cai <mandi@biobright.org>
2019-02-10 20:52:43 -0500

    Add data, first draft of timeline

--- js/index.js
+++ js/index.js
@@ -1,20 +1,57 @@
 let items = [
-  {"lane": 0, "id": "Qin", "start": moment().toDate(), "end": moment().add(4, 'weeks').toDate()},
+  { 'lane': 0, 'id': 'Chlamydia', 'start': moment().add(1, 'days').toDate(), 'end': moment().add(5, 'days').toDate(), 'source': 'STDcheck' },
+  { 'lane': 0, 'id': 'Chlamydia', 'start': moment().add(2, 'weeks').toDate(), 'end': moment().add(4, 'weeks').toDate(), 'source': 'The STD Project' },
+  { 'lane': 0, 'id': 'Chlamydia', 'start': moment().add(5, 'days').toDate(), 'end': moment().add(2, 'weeks').toDate(), 'source': 'North Dakota Department of Health' },
+  { 'lane': 0, 'id': 'Chlamydia', 'start': moment().add(2, 'weeks').toDate(), 'end': moment().add(2, 'weeks').toDate(), 'source': 'BC Centre for Disease Control' },
+  { 'lane': 1, 'id': 'Gonorrhea', 'start': moment().add(2, 'days').toDate(), 'end': moment().add(6, 'days').toDate(), 'source': 'STDcheck' },
+  { 'lane': 1, 'id': 'Gonorrhea', 'start': moment().add(1, 'weeks').toDate(), 'end': moment().add(3, 'weeks').toDate(), 'source': 'The STD Project' },
+  { 'lane': 1, 'id': 'Gonorrhea', 'start': moment().add(5, 'days').toDate(), 'end': moment().add(2, 'weeks').toDate(), 'source': 'North Dakota Department of Health' },
+  { 'lane': 1, 'id': 'Gonorrhea', 'start': moment().add(1, 'weeks').toDate(), 'end': moment().add(1, 'weeks').toDate(), 'source': 'BC Centre for Disease Control' },
+  { 'lane': 2, 'id': 'Syphilis', 'start': moment().add(3, 'weeks').toDate(), 'end': moment().add(6, 'weeks').toDate(), 'source': 'STDcheck' },
+  { 'lane': 2, 'id': 'Syphilis', 'start': moment().add(4, 'weeks').toDate(), 'end': moment().add(12, 'weeks').toDate(), 'source': 'The STD Project' },
+  { 'lane': 2, 'id': 'Syphilis', 'start': moment().add(1, 'weeks').toDate(), 'end': moment().add(2, 'weeks').toDate(), 'source': 'North Dakota Department of Health' },
+  { 'lane': 2, 'id': 'Syphilis', 'start': moment().add(3, 'weeks').toDate(), 'end': moment().add(4, 'weeks').toDate(), 'source': 'BC Centre for Disease Control' },
+  { 'lane': 3, 'id': 'HIV', 'start': moment().add(1, 'months').toDate(), 'end': moment().add(3, 'months').toDate(), 'source': 'STDcheck' },
+  { 'lane': 3, 'id': 'HIV', 'start': moment().add(4, 'weeks').toDate(), 'end': moment().add(12, 'weeks').toDate(), 'source': 'The STD Project' },
+  { 'lane': 3, 'id': 'HIV', 'start': moment().add(23, 'days').toDate(), 'end': moment().add(90, 'days').toDate(), 'source': 'North Dakota Department of Health' },
+  { 'lane': 3, 'id': 'HIV', 'start': moment().add(6, 'weeks').toDate(), 'end': moment().add(6, 'weeks').toDate(), 'source': 'BC Centre for Disease Control' },
+  { 'lane': 4, 'id': 'Hepatitis A', 'start': moment().add(2, 'weeks').toDate(), 'end': moment().add(7, 'weeks').toDate(), 'source': 'STDcheck' },
+  { 'lane': 4, 'id': 'Hepatitis A', 'start': moment().add(4, 'weeks').toDate(), 'end': moment().add(6, 'weeks').toDate(), 'source': 'The STD Project' },
+  { 'lane': 4, 'id': 'Hepatitis A', 'start': moment().add(4, 'weeks').toDate(), 'end': moment().add(4, 'weeks').toDate(), 'source': 'BC Centre for Disease Control' },
+  { 'lane': 5, 'id': 'Hepatitis B', 'start': moment().add(3, 'weeks').toDate(), 'end': moment().add(6, 'weeks').toDate(), 'source': 'STDcheck' },
+  { 'lane': 5, 'id': 'Hepatitis B', 'start': moment().add(4, 'weeks').toDate(), 'end': moment().add(10, 'weeks').toDate(), 'source': 'The STD Project' },
+  { 'lane': 5, 'id': 'Hepatitis B', 'start': moment().add(1, 'weeks').toDate(), 'end': moment().add(9, 'weeks').toDate(), 'source': 'North Dakota Department of Health' },
+  { 'lane': 5, 'id': 'Hepatitis B', 'start': moment().add(4, 'weeks').toDate(), 'end': moment().add(4, 'weeks').toDate(), 'source': 'BC Centre for Disease Control' },
+  { 'lane': 6, 'id': 'Hepatitis C', 'start': moment().add(8, 'weeks').toDate(), 'end': moment().add(9, 'weeks').toDate(), 'source': 'STDcheck' },
+  { 'lane': 6, 'id': 'Hepatitis C', 'start': moment().add(6, 'weeks').toDate(), 'end': moment().add(20, 'weeks').toDate(), 'source': 'The STD Project' },
+  { 'lane': 6, 'id': 'Hepatitis C', 'start': moment().add(4, 'weeks').toDate(), 'end': moment().add(10, 'weeks').toDate(), 'source': 'North Dakota Department of Health' },
+  { 'lane': 6, 'id': 'Hepatitis C', 'start': moment().add(2, 'weeks').toDate(), 'end': moment().add(2, 'weeks').toDate(), 'source': 'BC Centre for Disease Control' },
+  { 'lane': 7, 'id': 'Oral Herpes', 'start': moment().add(4, 'weeks').toDate(), 'end': moment().add(6, 'weeks').toDate(), 'source': 'STDcheck' },
+  { 'lane': 7, 'id': 'Oral Herpes', 'start': moment().add(6, 'weeks').toDate(), 'end': moment().add(16, 'weeks').toDate(), 'source': 'The STD Project' },
+  { 'lane': 7, 'id': 'Oral Herpes', 'start': moment().add(3, 'months').toDate(), 'end': moment().add(3, 'months').toDate(), 'source': 'BC Centre for Disease Control' },
+  { 'lane': 8, 'id': 'Genital Herpes', 'start': moment().add(4, 'weeks').toDate(), 'end': moment().add(6, 'weeks').toDate(), 'source': 'STDcheck' },
+  { 'lane': 8, 'id': 'Genital Herpes', 'start': moment().add(6, 'weeks').toDate(), 'end': moment().add(16, 'weeks').toDate(), 'source': 'The STD Project' },
+  { 'lane': 8, 'id': 'Genital Herpes', 'start': moment().add(3, 'months').toDate(), 'end': moment().add(3, 'months').toDate(), 'source': 'BC Centre for Disease Control' },
+  { 'lane': 9, 'id': 'Trichomoniasis', 'start': moment().add(2, 'weeks').toDate(), 'end': moment().add(3, 'weeks').toDate(), 'source': 'The STD Project' },
+  { 'lane': 9, 'id': 'Trichomoniasis', 'start': moment().add(4, 'days').toDate(), 'end': moment().add(28, 'days').toDate(), 'source': 'BC Centre for Disease Control' },
+  { 'lane': 10, 'id': 'HPV', 'start': moment().add(4, 'months').toDate(), 'end': moment().add(6, 'months').toDate(), 'source': 'BC Centre for Disease Control' }
 ]
 
+let numberWeeks = 24
+
 let timeStart = moment().toDate(),
-  timeEnd = moment().add(52, 'weeks').toDate()
+  timeEnd = moment().add(numberWeeks, 'weeks').toDate()
 
 let margin = {
-    top: 20,
+    top: 30,
     right: 30,
     bottom: 30,
-    left: 30
+    left: 40
   },
   width = 550 - margin.right - margin.left,
-  height = 300 - margin.top - margin.bottom
-  lanes = ['Chlamydia', 'Gonorrhea', 'Syphilis', 'HIV', 'Hepatitis B'] // names of stds,
-  miniHeight = 50, // height of mini brushable timeline
+  height = 350 - margin.top - margin.bottom
+  lanes = ['Chlamydia', 'Gonorrhea', 'Syphilis', 'HIV', 'Hepatitis A', 'Hepatitis B', 'Hepatitis C', 'Oral Herpes', ' Genital Herpes', 'Trichomoniasis', 'HPV'] // names of stds,
+  miniHeight = 75, // height of mini brushable timeline
   mainHeight = height - miniHeight - 50 // height of bigger timeline
 
 // scales
@@ -42,6 +79,18 @@ svg.append('defs').append('clipPath')
   .append('rect')
   .attr('width', width)
   .attr('height', mainHeight)
+
+let timelineLabel = svg.append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + 5 + ')')
+
+timelineLabel.append('text')
+  .text('Time of possible contraction')
+  .attr('font-size', '7px')
+
+timelineLabel.append('text')
+  .attr('dx', 414)
+  .text(numberWeeks + ' weeks')
+  .attr('font-size', '7px')
 
 let main = svg.append('g')
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
@@ -113,6 +162,10 @@ let itemRects = main.append('g')
 
 let miniRectHeight = 5
 
+let sourceColors = d3.scaleOrdinal()
+  .domain(lanes)
+  .range(["rgb(91,88,143)", "rgb(152,154,202)", "rgb(35,34,66)", "rgb(198,103,243)", "rgb(46,13,147)", "rgb(251,9,152)", "rgb(121,35,103)", "rgb(241,115,177)", "rgb(45,109,249)", "rgb(236,130,46)", "rgb(93,24,0)"])
+
 //mini item rects
 mini.append('g').selectAll('.mini-items')
   .data(items)
@@ -124,27 +177,42 @@ mini.append('g').selectAll('.mini-items')
     return x(d.start)
   })
   .attr('y', function (d) {
-    return y2(d.lane + .5) - (miniRectHeight / 2)
+    return y2(d.lane) - (miniRectHeight + 1)
   })
   .attr('width', function (d) {
     return x(d.end) - x(d.start)
   })
   .attr('height', miniRectHeight)
+  .attr('fill', function(d) {
+    return sourceColors(d.id)
+  })
+  .attr('opacity', 0.6)
 
 //mini labels
-mini.append('g').selectAll('.mini-label')
-  .data(items)
-  .enter().append('text')
-  .attr('class', function (d) {
-    return 'mini-label ' + d.lane
-  })
-  .text(function (d) {
-    return d.id
-  })
-  .attr('x', function (d) {
-    return x(d.start)
-  })
-  .attr('y', function (d) {
-    return y2(d.lane + .5)
-  })
-  // .attr('dy', '2.0ex')
+// mini.append('g').selectAll('.mini-label')
+//   .data(items)
+//   .enter().append('text')
+//   .attr('class', function (d) {
+//     return 'mini-label ' + d.lane
+//   })
+//   .text(function (d) {
+//     return d.id
+//   })
+//   .attr('x', function (d) {
+//     return x(d.start)
+//   })
+//   .attr('y', function (d) {
+//     return y2(d.lane)
+//   })
+//   .attr('dy', '2.0ex')
+
+let timelineLines = svg.append('g')
+  .attr('transform', 'translate(' + margin.right + ',' + margin.top + ')')
+
+timelineLines.append('line')
+  .attr('x1', margin.left)
+  .attr('x2', margin.left)
+  .attr('y1', -margin.top + 12)
+  .attr('y2', mainHeight + miniHeight)
+  .attr('stroke', '#ff7e00')
+  .attr('stroke-dasharray', '3,3')
